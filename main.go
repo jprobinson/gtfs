884d4f58559661c3eefc97823f02d3b7ad6a3e0a
JP Robinson <john.paul.robinson@gmail.com>
2019-12-17 16:17:34 -0500

    progress

--- main.go
+++ main.go
@@ -2,33 +2,169 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 	"strings"
 )
 
 func main() {
-
 	// trip ID => route ID
-	// trips.txt
 	routesByTrip := getRoutesByTrip()
-
 	// train -> trip -> stop ID
-	// stop_times.txt
-	north, _ := getTripStops(routesByTrip)
-	var max []string
-	for _, stops := range north["L"] {
-		if len(stops) > len(max) {
-			fmt.Println("NEW MAX!")
-			max = stops
-			fmt.Println(max)
+	tripsByRoute := getTripStops(routesByTrip)
+	// add metadata like whoa
+	// train -> route -> []stops
+	stops := addStopData(tripsByRoute)
+
+	writeGoFile("routes", stops)
+	writeJSFile("routes", stops)
+
+	var synsOut []Synonym
+	for _, stop := range getStopData() {
+		synsOut = append(synsOut, Synonym{
+			Value:    stop.PhoneticName,
+			Synonyms: stop.Synonyms,
+		})
+	}
+	writeJSFile("synonyms", synsOut)
+
+	// phono Name => Line => stop ID
+	stopsOut := map[string]map[string]string{}
+	for line, route := range stops {
+		for _, stop := range route.Stops {
+			_, exists := stopsOut[stop.PhoneticName]
+			if !exists {
+				stopsOut[stop.PhoneticName] = map[string]string{}
+			}
+
+			stopsOut[stop.PhoneticName][line] = stop.ID
+
 		}
 	}
-	//	fmt.Printf("\n\n%#v", south["L"])
 }
 
-func getTripStops(trips map[string]string) (north map[string][]string, south map[string][]string) {
+func writeJSFile(name string, data interface{}) {
+	jsFile, err := os.Create(strings.ToLower(name) + ".json")
+	if err != nil {
+		fmt.Printf("unable to open %s.json file: %s\n", name, err)
+		os.Exit(1)
+	}
+	defer jsFile.Close()
+
+	enc := json.NewEncoder(jsFile)
+	enc.SetIndent("", "  ")
+	err = enc.Encode(data)
+	if err != nil {
+		fmt.Println("unable to write routes.json file: ", err)
+		os.Exit(1)
+	}
+}
+func writeGoFile(name string, data interface{}) {
+	goFile, err := os.Create(strings.ToLower(name) + ".go")
+	if err != nil {
+		fmt.Printf("unable to open %s.go file: %s\n", name, err)
+		os.Exit(1)
+	}
+	defer goFile.Close()
+	fmt.Fprintf(goFile, "package gtfs\n\nvar %s = %#v", strings.Title(name), data)
+}
+
+type (
+	Route struct {
+		Name string
+
+		Northbound string
+		Southbound string
+
+		Stops []Stop
+	}
+	Stop struct {
+		ID string
+
+		MTAName      string
+		DisplayName  string
+		PhoneticName string
+
+		Synonyms []string
+	}
+
+	Synonym struct {
+		Value    string   `json:"value"`
+		Synonyms []string `json:"synonyms"`
+	}
+)
+
+func addStopData(tripsByRoute map[string][]string) map[string]Route {
+	stopData := getStopData()
+
+	out := map[string]Route{}
+	for line, route := range tripsByRoute {
+		routeInfo := Route{
+			Name:       line,
+			Northbound: routes[line].Northbound,
+			Southbound: routes[line].Southbound,
+		}
+
+		for _, stopID := range route {
+			stop, ok := stopData[stopID]
+			if !ok {
+				fmt.Printf("we didnt find data for %s!", stopID)
+				os.Exit(1)
+			}
+
+			routeInfo.Stops = append(routeInfo.Stops, stop)
+		}
+
+		out[line] = routeInfo
+	}
+	return out
+}
+
+func getStopData() map[string]Stop {
+	stopsFile, err := os.Open("./static_gtfs/stops.txt")
+	if err != nil {
+		fmt.Println("unable to open stops.txt:", err)
+		os.Exit(1)
+	}
+	defer stopsFile.Close()
+
+	cols := map[string]int{}
+	stopData := map[string]Stop{}
+	r := csv.NewReader(stopsFile)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("unable to read stops.txt:", err)
+			os.Exit(1)
+		}
+		if len(cols) == 0 {
+			for idx, val := range record {
+				cols[val] = idx
+			}
+			continue
+		}
+
+		stopID := record[cols["stop_id"]]
+		mtaName := record[cols["stop_name"]]
+		displayName, phonoName, syns := makeStopNames(mtaName)
+		stopData[stopID] = Stop{
+			ID:           stopID,
+			MTAName:      mtaName,
+			DisplayName:  displayName,
+			PhoneticName: phonoName,
+			Synonyms:     syns,
+		}
+	}
+	return stopData
+}
+
+func getTripStops(trips map[string]string) map[string][]string {
 	stopsFile, err := os.Open("./static_gtfs/stop_times.txt")
 	if err != nil {
 		fmt.Println("unable to open stop_times.txt:", err)
@@ -38,8 +174,8 @@ func getTripStops(trips map[string]string) (north map[string][]string, south map
 
 	cols := map[string]int{}
 	// train -> trip -> stops
-	north = map[string]map[string][]string{}
-	south = map[string]map[string][]string{}
+	northTrips := map[string]map[string][]string{}
+	southTrips := map[string]map[string][]string{}
 	r := csv.NewReader(stopsFile)
 	for {
 		record, err := r.Read()
@@ -58,23 +194,46 @@ func getTripStops(trips map[string]string) (north map[string][]string, south map
 		}
 
 		train := trips[record[cols["trip_id"]]]
-		if _, ok := north[train]; !ok {
-			north[train] = map[string][]string{}
+		if _, ok := northTrips[train]; !ok {
+			northTrips[train] = map[string][]string{}
 		}
-		if _, ok := south[train]; !ok {
-			south[train] = map[string][]string{}
+		if _, ok := southTrips[train]; !ok {
+			southTrips[train] = map[string][]string{}
 		}
 
 		if strings.HasSuffix(record[cols["stop_id"]], "S") {
-			south[train][record[cols["trip_id"]]] = append(south[train][record[cols["trip_id"]]],
-				record[cols["stop_id"]])
+			stopID := strings.TrimSuffix(record[cols["stop_id"]], "S")
+			southTrips[train][record[cols["trip_id"]]] = append(southTrips[train][record[cols["trip_id"]]],
+				stopID)
 		} else {
-			north[train][record[cols["trip_id"]]] = append(north[train][record[cols["trip_id"]]],
-				record[cols["stop_id"]])
+			stopID := strings.TrimSuffix(record[cols["stop_id"]], "N")
+			northTrips[train][record[cols["trip_id"]]] = append(northTrips[train][record[cols["trip_id"]]],
+				stopID)
 		}
 	}
 
-	return north, south
+	// for each train, find the longest trip route
+	trainTrips := map[string][]string{}
+	for route, _ := range routes {
+		max := len(trainTrips[route])
+
+		for _, trips := range northTrips[route] {
+			if len(trips) > max {
+				for i := len(trips)/2 - 1; i >= 0; i-- {
+					opp := len(trips) - 1 - i
+					trips[i], trips[opp] = trips[opp], trips[i]
+				}
+				trainTrips[route] = trips
+				max = len(trips)
+			}
+		}
+		for _, trips := range southTrips[route] {
+			if len(trips) > max {
+				trainTrips[route] = trips
+			}
+		}
+	}
+	return trainTrips
 }
 
 func getRoutesByTrip() map[string]string {
@@ -112,21 +271,82 @@ func getRoutesByTrip() map[string]string {
 	return routesByTrip
 }
 
-type (
-	Route struct {
-		Name string
-
-		Northbound string
-		Southbound string
-
-		Trips [][]Stop
-	}
-	Stop struct {
-		ID       string
-		Name     string
-		Synonyms []string
-	}
+var (
+	reAvenue  = regexp.MustCompile("(Av)($| - |,| )")
+	reAvenues = regexp.MustCompile("(Avs)($| - |,| )")
+	reStreet  = regexp.MustCompile("(St)($| - |,| )")
+	reStreets = regexp.MustCompile("(Sts)($| - |,| )")
+	rePlace   = regexp.MustCompile("(Pl)($| - |,| )")
 )
+
+func makeStopNames(mtaName string) (string, string, []string) {
+	replacements := [][]string{
+		{" (", ", "},
+		{")", ""},
+		{"Hts", "Heights"},
+		{"Sq", "Square"},
+		{"Pkwy", "Parkway"},
+		{"Blvd", "Boulevard"},
+		{"Hwy", "Highway"},
+		{"Ctr", "Center"},
+		{"Pkwy", "Parkway"},
+		{"1 ", "1st "},
+		{"2 ", "2nd "},
+		{"2-", "2nd "},
+		{"3 ", "3rd "},
+		{"4 ", "4th "},
+		{"4-", "4th "},
+		{"5 ", "5th "},
+		{"6 ", "6th "},
+		{"7 ", "7th "},
+		{"7-", "7th "},
+		{"8 ", "8th "},
+		{"9 ", "9th "},
+		{"0 ", "0th "},
+		{"E ", "East "},
+		{"W ", "West "},
+		{"N ", "North "},
+		{"S ", "South "},
+		{"/", ", "},
+	}
+
+	displayName := mtaName
+	for _, rep := range replacements {
+		displayName = strings.ReplaceAll(displayName, rep[0], rep[1])
+	}
+
+	displayName = fixStAve(reAvenue, displayName, "Avenue")
+	displayName = fixStAve(reAvenues, displayName, "Avenues")
+	displayName = fixStAve(reStreet, displayName, "Street")
+	displayName = fixStAve(reStreets, displayName, "Streets")
+	displayName = fixStAve(rePlace, displayName, "Place")
+	displayName = strings.Join(strings.Fields(displayName), " ")
+
+	syn := strings.ReplaceAll(displayName, " - ", ", ")
+	syns := strings.Split(syn, ",")
+	for i, syn := range syns {
+		syns[i] = strings.TrimSpace(syn)
+	}
+	if len(syns) == 2 {
+		syns = append(syns, syns[1]+", "+syns[0])
+	}
+	if syn != displayName {
+		syns = append(syns, syn)
+	}
+	return displayName, syn, syns
+}
+
+func fixStAve(re *regexp.Regexp, given, want string) string {
+	return re.ReplaceAllStringFunc(given, func(found string) string {
+		rep := want
+		if strings.Contains(found, "-") || strings.Contains(found, ",") {
+			rep += ","
+		}
+		rep += " "
+		fmt.Println(found + "->" + rep)
+		return rep
+	})
+}
 
 var routes = map[string]Route{
 	"1":  {Northbound: "Bronx", Southbound: "South&nbsp;Ferry"},
